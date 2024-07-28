c73bcdddee0b516c470747bc3c58868ef4f217e3
rowlow302606 <rowlow302606@gmail.com>
2024-07-28 18:24:48 +0900

    second commit

---
+++ introdunction-to-html/getting-started-with-html/scripts/main.js
@@ -0,0 +1,7 @@
+const noWhitespace = 
+document.getElementById("noWhitespace").innerHTML;
+console.log(noWhitespace);
+
+const whitespace = 
+document.getElementById("whitespace").innerHTML;
+console.log(whitespace);
